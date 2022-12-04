1afd66ec9a93dbd2456ccd41d9b185514667c633
Rinal Shaihutdinov <iversonmelo.6@yandex.ru>
2022-12-04 15:02:06 +0300

    Initial comit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 2500;
+
+        int bonus = (amount / 20);
+
+        System.out.println("Миль(и):" + bonus);
+
+    }
+    
+}
